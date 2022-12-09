132966323cc4d54fde0a834b74cae8c43b7e137a
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-09 13:36:16 -0800

    Remove temp HTML, create console logs to test API fetch. Create variables for data required for Schedule page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
 //temp html variables to be replaced by bootstrapped frontend
 
-var gameHeader = document.querySelector("#header");
-var gameName = document.querySelector("#game");
-var gameTeam1 = document.querySelector("#team1");
-var gameTeam2 = document.querySelector("#team2");
+var gameHeaderHTML = document.querySelector("#header");
+var gameNameHTML = document.querySelector("#game");
+var gameTeam1HTML = document.querySelector("#team1");
+var gameTeam2HTML = document.querySelector("#team2");
 
-
-// Print current scoreboard data for current week
+// Schedule function for current scoreboard data for current week
 function printResults(resultObj) {
-gameName.textContent = resultObj.name;
-gameTeam1.textContent = resultObj.competitions[0].competitors[0].team.displayName;
-gameTeam2.textContent = resultObj.competitions[0].competitors[1].team.displayName + " score: " + resultObj.competitions[0].competitors[1].score;
+  var gameWeek = resultObj.week.number;
+  var gameName = resultObj.name;
+  var gameTeam1 = resultObj.competitions[0].competitors[0].team.displayName;
+  var gameTeam2 = resultObj.competitions[0].competitors[1].team.displayName;
+  var gameTeam1Score = resultObj.competitions[0].competitors[0].score;
+  var gameTeam2Score = resultObj.competitions[0].competitors[1].score;
+  var gameStatus = resultObj.status.type.shortDetail;
+
+  console.log("Week " + gameWeek + " of 18");
+  console.log(gameName);
+  console.log(gameTeam1 + " " + gameTeam1Score);
+  console.log(gameTeam2 + " " + gameTeam2Score);
+  console.log(gameStatus);
 }
 
 // Get current scoreboard data for current week
 function getApi() {
-  var requestUrl = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard";
+  var requestUrl =
+    "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard";
 
   fetch(requestUrl)
     .then(function (response) {
@@ -24,7 +34,6 @@ function getApi() {
     .then(function (data) {
       console.log(data);
       printResults(data.events[0]);
-      console.log(data.events[0]);
     });
 }
 
