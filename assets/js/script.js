955671d896a3c21955946de86a5a4698e1a4c4d9
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-09 14:02:19 -0800

    Create additional variables for game data including: Home and away, score, logo url

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,35 @@ var gameTeam2HTML = document.querySelector("#team2");
 
 // Schedule function for current scoreboard data for current week
 function printResults(resultObj) {
+  // Week number
   var gameWeek = resultObj.week.number;
+
+  // Game header data
   var gameName = resultObj.name;
-  var gameTeam1 = resultObj.competitions[0].competitors[0].team.displayName;
-  var gameTeam2 = resultObj.competitions[0].competitors[1].team.displayName;
-  var gameTeam1Score = resultObj.competitions[0].competitors[0].score;
-  var gameTeam2Score = resultObj.competitions[0].competitors[1].score;
   var gameStatus = resultObj.status.type.shortDetail;
+
+  // Team 1
+  var gameTeam1Name = resultObj.competitions[0].competitors[0].team.displayName;
+  var gameTeam1Logo = resultObj.competitions[0].competitors[0].team.logo;
+  var gameTeam1Score = resultObj.competitions[0].competitors[0].score;
+  var gameTeam1HomeAway = resultObj.competitions[0].competitors[0].homeAway;
+  var gameTeam1Winner = resultObj.competitions[0].competitors[0].winner;
+
+  // Team 2
+  var gameTeam2Name = resultObj.competitions[0].competitors[1].team.displayName;
+  var gameTeam2Logo = resultObj.competitions[0].competitors[1].team.logo;
+  var gameTeam2Score = resultObj.competitions[0].competitors[1].score;
+  var gameTeam2HomeAway = resultObj.competitions[0].competitors[1].homeAway;
+  var gameTeam2Winner = resultObj.competitions[0].competitors[1].winner;
 
   console.log("Week " + gameWeek + " of 18");
   console.log(gameName);
-  console.log(gameTeam1 + " " + gameTeam1Score);
-  console.log(gameTeam2 + " " + gameTeam2Score);
+  console.log(
+    gameTeam1Name + " " + gameTeam1Score + " logo link: " + gameTeam1Logo
+  );
+  console.log(
+    gameTeam2Name + " " + gameTeam2Score + " logo link: " + gameTeam2Logo
+  );
   console.log(gameStatus);
 }
 
