bba2d533c32b72aa730bda71bb647de3e2339b3b
kdmoreno14-LNflrw <kdmoreno14@gmail.com>
2022-12-12 11:18:48 -0800

    asdfasdfadsf

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ function printResults(resultObj) {
   var gameName = resultObj.name;
   var gameStatus = resultObj.status.type.shortDetail;
   var gameSummaryLink = resultObj.links[0].href;
+  var gameTime = resultObj.competitions[0].type.shortDetail
 
   // Team 1
   var gameTeam1Name = resultObj.competitions[0].competitors[0].team.displayName;
@@ -71,3 +72,55 @@ function getApi() {
 }
 
 getApi();
+
+
+//Used to populate schedule 
+let html = '';
+let kek = data;
+
+function schedulePop()  {
+    kek.foreach( kek => {
+      html += `<div class="matchups">
+      <div class="row">
+        <div class="col-4 mt-4">
+            <h2 class="home team">${gameTeam1Name}</h2>
+            <img class="logo" src="${gameTeam1Logo}" >Team Logo</img>
+        </div>
+        <div class="col-4 middle">
+            <div class="row">
+                <h5 class="time">${gameTime}</h5>
+            </div>
+            <div class="row">
+                <h5 class="result">score</h5>
+            </div>
+            <div class="row">
+                <h5 class="odds">Rams +1500</h5>
+            </div>         
+        </div>
+        <div class="col-4 mt-4">
+            <h2 class="away team">${gameTeam2Name}</h2>
+            <img class="logo" src="${gameTeam2Logo}" >Team Logo</img>
+        </div>
+      </div>
+      `;
+    });
+  
+}
+
+function standingsPop() {
+  top.foreach( top => {
+      html += `
+
+      `;
+  });
+
+}
+
+function statsPop() {
+  topkek.foreach( topkek => {
+    html += `
+    
+    `;
+  });
+}
+
