636f2a62df5a9833af9675a36cc95b06697691b7
kdmoreno14-LNflrw <kdmoreno14@gmail.com>
2022-12-12 15:10:12 -0800

    for loop works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var scheduleContainer = document.querySelector('.games-container')
 // Schedule function for current scoreboard data for current week
 function printResults(resultObj) {
   // Week number
@@ -48,7 +49,7 @@ function printResults(resultObj) {
   console.log(gameTeam2Name + " win? " + gameTeam2Winner);
   console.log("Game Summary: " + gameSummaryLink);
 }
-
+let html = '';
 // Get current scoreboard data for current week
 function getApi() {
   var requestUrl =
@@ -59,16 +60,48 @@ function getApi() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.events);
+      console.log(data);
       printResults(data.events[0]);
-      
+      html += `<div class="matchups">`
       // For Each
       data.events.forEach((element) => {
         var gameTeam1Name = element.competitions[0].competitors[0].team.displayName;
-        console.log(element);
-        console.log(gameTeam1Name);
+        var gameTeam2Name = element.competitions[0].competitors[1].team.displayName;
+        var gameTeam1Logo = element.competitions[0].competitors[0].team.logo;
+        var gameTeam2Logo = element.competitions[0].competitors[1].team.logo;
+        var gameTime = element.competitions[0].status.type.shortDetail
+        var gameTeam1Score = element.competitions[0].competitors[0].score;
+        var gameTeam2Score = element.competitions[0].competitors[1].score;
+
+        console.log(gameTeam1Name)
+        html += `
+        <div class="row myrow">
+            <div class="col-4 mt-4">
+              <h2 class="home team">${gameTeam1Name}</h2>
+              <img class="logo" src="${gameTeam1Logo}">
+            </div>
+            <div class="col-4 d-flex flex-column justify-content-around middle">
+              <div class="row">
+                <h2 class="time">${gameTime}</h2>
+              </div>
+              <div class="row">
+                <h2 class="result">${gameTeam1Score}  -  ${gameTeam2Score}</h2>
+              </div>
+              <div class="row">
+                <h2 class="odds"></h2>
+              </div>         
+          </div>
+          <div class="col-4 mt-4">
+            <h2 class="away team">${gameTeam2Name}</h2>
+            <img class="logo" src="${gameTeam2Logo}" >
+          </div>
+          </div>
+        `;
+        
       });
-    });
+      html += "</div>"
+      $(".games-container").html(html);
+    });;
 }
 
 // Display today's date in header
@@ -80,53 +113,53 @@ $("#today").text(todaysDate);
 getApi();
 
 
-//Used to populate schedule 
-let html = '';
-let kek = data;
+// //Used to populate schedule 
+// 
+// let kek = data;
 
-function schedulePop()  {
-    kek.foreach( kek => {
-      html += `<div class="matchups">
-      <div class="row">
-        <div class="col-4 mt-4">
-            <h2 class="home team">${gameTeam1Name}</h2>
-            <img class="logo" src="${gameTeam1Logo}" >Team Logo</img>
-        </div>
-        <div class="col-4 middle">
-            <div class="row">
-                <h5 class="time">${gameTime}</h5>
-            </div>
-            <div class="row">
-                <h5 class="result">score</h5>
-            </div>
-            <div class="row">
-                <h5 class="odds">Rams +1500</h5>
-            </div>         
-        </div>
-        <div class="col-4 mt-4">
-            <h2 class="away team">${gameTeam2Name}</h2>
-            <img class="logo" src="${gameTeam2Logo}" >Team Logo</img>
-        </div>
-      </div>
-      `;
-    });
+// function schedulePop()  {
+//     kek.foreach( kek => {
+//       html += `<div class="matchups">
+//       <div class="row">
+//         <div class="col-4 mt-4">
+//             <h2 class="home team">${gameTeam1Name}</h2>
+//             <img class="logo" src="${gameTeam1Logo}" >Team Logo</img>
+//         </div>
+//         <div class="col-4 middle">
+//             <div class="row">
+//                 <h5 class="time">${gameTime}</h5>
+//             </div>
+//             <div class="row">
+//                 <h5 class="result">score</h5>
+//             </div>
+//             <div class="row">
+//                 <h5 class="odds">Rams +1500</h5>
+//             </div>         
+//         </div>
+//         <div class="col-4 mt-4">
+//             <h2 class="away team">${gameTeam2Name}</h2>
+//             <img class="logo" src="${gameTeam2Logo}" >Team Logo</img>
+//         </div>
+//       </div>
+//       `;
+//     });
   
-}
+// }
 
-function standingsPop() {
-  top.foreach( top => {
-      html += `
+// function standingsPop() {
+//   top.foreach( top => {
+//       html += `
 
-      `;
-  });
+//       `;
+//   });
 
-}
+// }
 
-function statsPop() {
-  topkek.foreach( topkek => {
-    html += `
+// function statsPop() {
+//   topkek.foreach( topkek => {
+//     html += `
     
-    `;
-  });
-}
+//     `;
+//   });
+// }
 
