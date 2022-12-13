72820157e41408d25e61c7cb9d281869581893a2
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-12 20:15:03 -0800

    Center week number and today's date under header links. Clean up comments and code in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,19 @@
-var scheduleContainer = document.querySelector('.games-container')
-// Schedule function for current scoreboard data for current week
-function printResults(resultObj) {
-  // Week number
-  var gameWeek = resultObj.week.number;
+// Container for appending game info such as "teams" "scores" "schedules" etc.
+var scheduleContainer = document.querySelector(".games-container");
 
-  // Game general data
-  var gameName = resultObj.name;
-  var gameStatus = resultObj.status.type.shortDetail;
-  var gameSummaryLink = resultObj.links[0].href;
-  var gameTime = resultObj.competitions[0].type.shortDetail
+// Display today's date in header
+var todaysDate = dayjs().format("MMM D, YYYY");
+$("#today").text(todaysDate);
 
-  // Team 1
-  var gameTeam1Name = resultObj.competitions[0].competitors[0].team.displayName;
-  var gameTeam1Logo = resultObj.competitions[0].competitors[0].team.logo;
-  var gameTeam1Score = resultObj.competitions[0].competitors[0].score;
-  var gameTeam1HomeAway = resultObj.competitions[0].competitors[0].homeAway;
-  var gameTeam1Winner = resultObj.competitions[0].competitors[0].winner;
-
-  // Team 2
-  var gameTeam2Name = resultObj.competitions[0].competitors[1].team.displayName;
-  var gameTeam2Logo = resultObj.competitions[0].competitors[1].team.logo;
-  var gameTeam2Score = resultObj.competitions[0].competitors[1].score;
-  var gameTeam2HomeAway = resultObj.competitions[0].competitors[1].homeAway;
-  var gameTeam2Winner = resultObj.competitions[0].competitors[1].winner;
-
-  console.log("Week " + gameWeek + " of 18");
-  console.log(gameName);
-  console.log(
-    gameTeam1HomeAway +
-      " " +
-      gameTeam1Name +
-      " " +
-      gameTeam1Score +
-      " logo link: " +
-      gameTeam1Logo
-  );
-  console.log(
-    gameTeam2HomeAway +
-      " " +
-      gameTeam2Name +
-      " " +
-      gameTeam2Score +
-      " logo link: " +
-      gameTeam2Logo
-  );
-  console.log(gameStatus);
-  console.log(gameTeam1Name + " win? " + gameTeam1Winner);
-  console.log(gameTeam2Name + " win? " + gameTeam2Winner);
-  console.log("Game Summary: " + gameSummaryLink);
+// Display current week number in header
+function printWeek(weekNumber) {
+  $("#week-number").text("Week " + weekNumber + " of 18");
 }
-let html = '';
+
+// variable for use in "for each" function below
+let html = "";
+
+
 // Get current scoreboard data for current week
 function getApi() {
   var requestUrl =
@@ -60,20 +24,28 @@ function getApi() {
       return response.json();
     })
     .then(function (data) {
+      
+      // example of data from API
       console.log(data);
-      printResults(data.events[0]);
-      html += `<div class="matchups">`
-      // For Each
+      
+      // Get week number and append in header
+      printWeek(data.week.number);
+
+      // For each game event in API create info card in html
+      // Variables below are specific data parsed from API fetch for use on page
+      html += `<div class="matchups">`;
       data.events.forEach((element) => {
-        var gameTeam1Name = element.competitions[0].competitors[0].team.displayName;
-        var gameTeam2Name = element.competitions[0].competitors[1].team.displayName;
+        var gameTeam1Name =
+          element.competitions[0].competitors[0].team.displayName;
+        var gameTeam2Name =
+          element.competitions[0].competitors[1].team.displayName;
         var gameTeam1Logo = element.competitions[0].competitors[0].team.logo;
         var gameTeam2Logo = element.competitions[0].competitors[1].team.logo;
-        var gameTime = element.competitions[0].status.type.shortDetail
+        var gameTime = element.competitions[0].status.type.shortDetail;
         var gameTeam1Score = element.competitions[0].competitors[0].score;
         var gameTeam2Score = element.competitions[0].competitors[1].score;
 
-        console.log(gameTeam1Name)
+        console.log(gameTeam1Name);
         html += `
         <div class="row myrow">
             <div class="col-4 mt-4">
@@ -97,69 +69,13 @@ function getApi() {
           </div>
           </div>
         `;
-        
       });
-      html += "</div>"
+      html += "</div>";
+
+      // set html on page with created html / values from above
       $(".games-container").html(html);
-    });;
+    });
 }
-
-// Display today's date in header
-
-var todaysDate = dayjs().format('MMM D, YYYY');
-$("#today").text(todaysDate);
 
 // get API data upon page load
 getApi();
-
-
-// //Used to populate schedule 
-// 
-// let kek = data;
-
-// function schedulePop()  {
-//     kek.foreach( kek => {
-//       html += `<div class="matchups">
-//       <div class="row">
-//         <div class="col-4 mt-4">
-//             <h2 class="home team">${gameTeam1Name}</h2>
-//             <img class="logo" src="${gameTeam1Logo}" >Team Logo</img>
-//         </div>
-//         <div class="col-4 middle">
-//             <div class="row">
-//                 <h5 class="time">${gameTime}</h5>
-//             </div>
-//             <div class="row">
-//                 <h5 class="result">score</h5>
-//             </div>
-//             <div class="row">
-//                 <h5 class="odds">Rams +1500</h5>
-//             </div>         
-//         </div>
-//         <div class="col-4 mt-4">
-//             <h2 class="away team">${gameTeam2Name}</h2>
-//             <img class="logo" src="${gameTeam2Logo}" >Team Logo</img>
-//         </div>
-//       </div>
-//       `;
-//     });
-  
-// }
-
-// function standingsPop() {
-//   top.foreach( top => {
-//       html += `
-
-//       `;
-//   });
-
-// }
-
-// function statsPop() {
-//   topkek.foreach( topkek => {
-//     html += `
-    
-//     `;
-//   });
-// }
-
