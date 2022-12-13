274778850ff0a5879805e7e2aaf30fd88be02ad0
mar <rojanomariano@gmail.com>
2022-12-13 10:36:32 -0800

    whatever

--- stats.js
+++
@@ -1,6 +0,0 @@
-// Buttons on cards will change to "favorited" and change color when clicked. 
-const favoriteButton = document.getElementById('.fav-btn');
-favoriteButton.addEventListener('click', () => {
-  favoriteButton.innerText = 'Favorited!';
-  favoriteButton.style.backgroundColor = '#00C853';
-});
